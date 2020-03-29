c4ce1f649079d871be55f73358c338d840f548dc
Grizly <burichin7@gmail.com>
2020-03-29 19:56:31 +0600

    create scroll 2

--- #source/main.js
+++ #source/main.js
@@ -1,1 +1,17 @@
 // Плавный скролл
+$(document).ready(function () {
+	let scroll = new SmoothScroll('a[href*="#section-1"]', {
+		speed: 500,
+		speedAsDuration: true
+	});
+
+	let scroll2 = new SmoothScroll('a[href*="#section-2"]', {
+		speed: 500,
+		speedAsDuration: true
+	});
+
+	let scroll3 = new SmoothScroll('a[href*="#section-3"]', {
+		speed: 500,
+		speedAsDuration: true
+	});
+});
